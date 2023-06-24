2e60e16531c91e3e536e1d058f3aafb77e2da3e0
Juan Guaqueta <jguaquet@emeal.nttdata.com>
2023-06-24 15:17:08 -0500

    mensaje de amor

--- index.php
+++ index.php
@@ -1,45 +1,28 @@
-<!-- login.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Mensaje de Amor</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #ffcce6;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        .message {
+            font-size: 24px;
+            color: #cc0066;
+            margin: 20px;
+        }
+    </style>
 </head>
 <body>
-
-<?php
-// Datos de ejemplo (en un sistema real, debes obtener estos datos de una base de datos)
-$username = 'user';
-$password = 'password';
-
-// Comprueba si el formulario ha sido enviado
-if (isset($_POST['submit'])) {
-    // Obtiene los datos ingresados por el usuario
-    $inputUsername = $_POST['username'];
-    $inputPassword = $_POST['password'];
-
-    // Comprueba si los datos ingresados son válidos
-    if ($inputUsername === $username && $inputPassword === $password) {
-        session_start();
-        $_SESSION['user'] = $username;
-        echo 'Login successful';
-        // Aquí podrías redireccionar a otra página, por ejemplo:
-        // header('Location: dashboard.php');
-    } else {
-        echo 'Invalid login credentials';
-    }
-}
-?>
-
-<form action="" method="post">
-    <label for="username">Username:</label>
-    <input type="text" name="username" required>
-    <br>
-    <label for="password">Password:</label>
-    <input type="password" name="password" required>
-    <br>
-    <input type="submit" name="submit" value="Login">
-</form>
-
+    <div class="message">
+        <?php
+            echo "¡Eres la luz que ilumina mi corazón!";
+        ?>
+    </div>
 </body>
 </html>
